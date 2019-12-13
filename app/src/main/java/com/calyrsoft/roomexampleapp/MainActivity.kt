3eed729f4f77d53585c242208ba0ed8f3a86e911
calyr <calyr.software@gmail.com>
2019-12-13 11:46:20 -0400

    Add Recycler View

--- app/src/main/java/com/calyrsoft/roomexampleapp/MainActivity.kt
+++ app/src/main/java/com/calyrsoft/roomexampleapp/MainActivity.kt
@@ -2,11 +2,18 @@ package com.calyrsoft.roomexampleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
---
+++ app/src/main/java/com/calyrsoft/roomexampleapp/WordListAdapter.kt
@@ -0,0 +1,33 @@
+package com.calyrsoft.roomexampleapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
+
+class WordListAdapter internal constructor(
+    context: Context
+) : RecyclerView.Adapter<WordListAdapter.WordViewHolder>(){
+
+    private val inflater: LayoutInflater = LayoutInflater.from(context)
+    private var words = emptyList<Word>()
+
+    inner class WordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val wordItemView: TextView = itemView.findViewById(R.id.textView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
+        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
+        return WordViewHolder(itemView)
+    }
+
+    override fun getItemCount() = words.size
+
+    override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
+        val current = words[position]
+        holder.wordItemView.text = current.word
+    }
+}
